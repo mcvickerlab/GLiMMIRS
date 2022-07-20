bde1d7bebddff6973fe239965d628424df655a2a
zrcjessica <jlz014@eng.ucsd.edu>
2022-07-19 18:20:34 -0700

    updated simulation script to write to h5

---
+++ scripts/run_sim_counts.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+#$ -V
+#$ -cwd
+
+# outdir=$HOME/crisprqtl_sim/sim_data
+outdir=$HOME/crisprQTL/simulated_data
+
+mkdir -p $outdir
+
+Rscript $PWD/sim_counts.R --out $outdir
+
+# message the user on slack if possible
+exit_code="$?"
+if command -v 'slack' &>/dev/null; then
+    if [ "$exit_code" -eq 0 ]; then
+		slack "sim_counts finished successfully" &>/dev/null
+	else
+		slack "sim_counts exited with error code $exit_code"
+	fi
+fi
+exit "$exit_code"
