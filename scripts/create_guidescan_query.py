32d66ee2107ca4bedaa81846c1ea21d5827f5607
somet3000 <karthikguruvayurappan@gmail.com>
2022-08-17 17:34:38 -0700

    Added cell cycle scoring notebook

--- scripts/create_guidescan_query.py
+++ scripts/create_guidescan_query.py
@@ -1,9 +1,7 @@
 import pandas as pd
 
 # read in file containing guide spacer sequences from Gasperini 2019 paper
-guide_sequences = pd.read_csv('/iblm/netapp/data1/external/Gasperini2019/suppl/GSE120861_grna_groups.at_scale.txt',
-                              sep = '\t',
-                              names = ['guide_group', 'guide_spacer'])
+guide_sequences = pd.read_csv('/iblm/netapp/data1/external/Gasperini2019/gasperini_2019_suppl_table_2.csv')
 
-guide_sequences['guide_spacer'] = guide_sequences['guide_spacer'] + 'NGG'
-guide_sequences['guide_spacer'].to_csv('/iblm/netapp/home/karthik/GuideScan/Gasperini2019/guide_sequences.csv', index = False)
+guide_sequences['Spacer'] = guide_sequences['Spacer'] + 'NGG'
+guide_sequences['Spacer'].to_csv('/iblm/netapp/home/karthik/GuideScan/Gasperini2019/guide_sequences.csv', index = False)
