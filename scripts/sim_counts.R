d4e5e0a46a1b5f27ac0255bcf594928e73031a21
zrcjessica <jlz014@eng.ucsd.edu>
2022-08-19 11:45:08 -0700

    new comments to data simulations script and notebook for testing model on simulated data

--- scripts/sim_counts.R
+++ scripts/sim_counts.R
@@ -25,7 +25,7 @@ parser$add_argument("--cells", action = "store", type = 'integer',
 	help = "number of cells in simualted dataset")
 parser$add_argument("--d", action = "store", type = "integer",
 	default = 2,
-	help = "number of gRNAs for each target")
+	help = "number of gRNAs for each target site (candidate enhancer)")
 parser$add_argument("--lambda", action = "store", type = "double",
 	default = 15,
 	help = "lambda parameter for Poisson distribution to draw number of guides per cell")
@@ -36,12 +36,14 @@ args <- parser$parse_args()
 
 nGuides = args$d * args$targets
 
-#######################################
+##############################################################
 #  simulated baseline (beta0)
 #  one beta per gene
-#######################################
+#  rnorm params obtained by fitting to experimental data
+##############################################################
 baselines <- rnorm(args$genes, mean = 2.24, sd = 1.8)
 
+# plot simulated values of beta0 (baseline)
 png(file.path(args$out, "hist_beta0.png"))
 hist(baselines)
 dev.off()
@@ -49,15 +51,16 @@ dev.off()
 #######################################
 #  assign guides to cells
 #  store as one hot encoding
+#  simulate guide efficiencies 
 #######################################
 
 # initialize one hot encoding
 onehot.guides <- matrix(0, args$cells, nGuides)
 
-# get guides per cell
+# get # of guides per cell
 guides.per.cell <- rpois(args$cells, args$lambda)
 
-# plot
+# plot # of guides per cell
 png(file.path(args$out, "hist_guides_per_cell.png"))
 hist(guides.per.cell, xlab = "guides per cell", ylab = "cells")  
 dev.off()
@@ -79,38 +82,39 @@ writeMM(Matrix(onehot.guides, sparse = TRUE),
 # assign guide efficiencies to guides in library
 efficiencies <- rbeta(nGuides, 6,3)
 
-# plot
+# plot guide efficiencies
 png(file.path(args$out, "hist_guide_efficiencies.png"))
 hist(efficiencies)
 dev.off()
 
 ####################################################
-#  assign target genes to candidate enhancers
+#  assign target genes to gRNAs
 ####################################################
 target.genes <- sample(1:args$genes, args$targets, replace = FALSE)
 guide.gene.map <- rep(target.genes, args$d)
 
-# get effect sizes of each enhancer
+# get effect sizes of each enhancer on its target gene 
 effect.sizes <- -1*(rgamma(args$targets, shape = 6, scale = 0.5))
 
 ####################################################
 #  get vector of beta1 values
 ####################################################
-beta1 <- numeric(args$genes)
+beta1 <- numeric(args$genes) # initialize vector of zeros
 
 for (i in 1:length(target.genes)) {
+    # for each target gene, update its position in beta1 to effect size
     target <- target.genes[i]
     beta1[target] <- effect.sizes[i]
 }
 
-# plot
+# plot beta1 values (expected pileup at zero)
 png(file.path(args$out, "hist_beta1.png"))
 hist(beta1)
 dev.off()
 
-####################################################
-#  define function for calculating X1 (per cell)
-####################################################
+############################################################################
+#  define a function for calculating X1 (different values for each gene) 
+############################################################################ 
 
 combined_prob <- function(cell, gene, verbose = FALSE) {
     if (verbose) {
@@ -135,7 +139,7 @@ combined_prob <- function(cell, gene, verbose = FALSE) {
 }
 
 ####################################################
-#  define all guide metadata to file
+#  write all guide metadata to file
 ####################################################
 
 # row index = gRNA identifier 
@@ -153,67 +157,74 @@ rmv.rand <- rmvnorm(n=args$cells, mean = c(0,0), sigma = cov.mtx)
 s.scores <- rmv.rand[,1]
 g2m.scores <- rmv.rand[,2]
 
-# plot as hex
+# plot S/G2M scores as hexbin
 png(file.path(args$out, "cell_cycle_scores_hexbin.png"))
 bin<-hexbin(s.scores, g2m.scores, xbins=50)
 plot(bin, main="Hexagonal Binning")
 dev.off()
 
+# write cell cycle scores (X2, X3) to file
 # row index = cell identifier
 cell.cycle.scores <- data.frame(s.scores, g2m.scores)
 write.table(cell.cycle.scores, file.path(args$out, "cell_cycle_scores.txt"), row.names = TRUE, quote = FALSE)
 
 ####################################################
-#  select beta2, beta3
+#  simulate beta2, beta3 (from gamma distr.)
 ####################################################
 beta2 <- rgamma(args$genes, shape = 6, scale = 0.5)
 beta3 <- rgamma(args$genes, shape =6, scale = 0.5)
 
-# plot
+# plot beta2
 png(file.path(args$out, "beta2_hist.png"))
 hist(beta2)
 dev.off()
 
+# plot beta3
 png(file.path(args$out, "beta3_hist.png"))
 hist(beta3)
 dev.off()
 
-####################################################
-#  write coeffs to file
-####################################################
+######################################################
+#  write coeffs to file (beta0, beta1, beta2, beta3)
+######################################################
 
 # row index = gene identifier
 coeffs <- data.frame(baselines, beta1, beta2, beta3)
 write.table(coeffs, file.path(args$out, "coeffs.txt"), row.names = TRUE, quote = FALSE)
 
 ####################################################
-#  scaling factors
+#  simulate scaling factors (poisson)
 ####################################################
+
+# simulate total counts per cell
 t.vec <- rpois(args$cells, 50000)
 
+# plot total counts per cell
 png(file.path(args$out, "t_pois_hist.png"))
 hist(t.vec)
 dev.off()
 
+# get scaling factors
 scaling.factors <- t.vec/1e6
 
-# plot
+# plot scaling factors 
 png(file.path(args$out, "scaling_factors_hist.png"))
 hist(scaling.factors)
 dev.off()
 
+# write scaling factors to file
 # row index = cell identifier
 write.table(data.frame(scaling.factors), file.path(args$out, "scaling_factors.txt"), 
     row.names = TRUE, quote = FALSE)
 
 ####################################################
-#  simulate data
+#  simulate counts matrix 
 ####################################################
 
 # initialize counts matrix
 sim.counts <-  matrix(0, args$genes, args$cells)
 
-# initialize matrix of x1 values
+# initialize matrix of x1 values (different set of values for each gene)
 x1.mtx <- matrix(0, args$genes, args$cells)
 
 # populate counts mtx by gene (by row)
@@ -224,10 +235,10 @@ for (gene in 1:args$genes) {
     b2 <- beta2[gene]
     b3 <- beta3[gene]
     
-    # initialize x1 as vector of zeros (assume it will not be affected by any guides in library)
+    # initialize x1 as vector of zeros (assume it is not affected by any gRNAs in library)
     x1 <- numeric(args$cells)
     
-    # check if enhancer of gene is targeted by any guides in our library
+    # check if enhancer of gene is targeted by any gRNAs
     if (gene %in% target.genes) {
         for (i in 1:args$cells) {
             x1[i] <- combined_prob(i, gene)
@@ -256,21 +267,21 @@ writeMM(Matrix(sim.counts), file.path(args$out, "counts.mtx"))
 writeMM(Matrix(x1.mtx), file.path(args$out, "x1.mtx"))
 
 ####################################################
-#  write data to h5
+#  write all data to h5
 ####################################################
 h5.path <- file.path(args$out, "sim.h5")
 h5createFile(h5.path)
 
 # write counts, chunked
 h5createDataset(h5.path, "counts", dim(sim.counts),
-    storage.mode = "integer", chunk=c(1000, 10000), level=7)
+    storage.mode = "integer", chunk=c(1000, 10000), level=5)
 
 h5write(sim.counts, h5.path, "counts")
 
 # write guide info
 h5createGroup(h5.path, "guides")
 h5createDataset(h5.path, "guides/one_hot", dim(onehot.guides),
-    storage.mode = "integer", chunk=c(1000, 1), level=7)
+    storage.mode = "integer", chunk=c(1000, 1), level=5)
 
 h5write(onehot.guides, h5.path,"guides/one_hot")
 h5write(guides.metadata, h5.path, "guides/metadata")
@@ -278,7 +289,7 @@ h5write(guides.metadata, h5.path, "guides/metadata")
 # write coeffs
 h5write(coeffs, h5.path, "coeffs")
 
-# write x
+# write x1
 h5createGroup(h5.path, "x")
 
 h5createDataset(h5.path, "x/x1", dim(x1.mtx),
