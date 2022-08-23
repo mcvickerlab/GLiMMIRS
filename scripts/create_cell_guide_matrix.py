cc6a4b6e221324c97792ee7660844e1a12ed0835
somet3000 <karthikguruvayurappan@gmail.com>
2022-08-23 15:07:27 -0700

    Added code to make H5 structure and fixed guide efficiency code mishap

--- scripts/create_cell_guide_matrix.py
+++ scripts/create_cell_guide_matrix.py
@@ -2,33 +2,19 @@
 import numpy as np
 import pandas as pd
 
-# file and command line packages
-import gzip 
-
-# string parsing
-import re
-
 # read in phenodata file
+print('reading in phenodata table')
 colnames = open('/iblm/netapp/data1/external/Gasperini2019/suppl/GSE120861_at_scale.phenoData.colnames.txt') \
            .read().splitlines()
 phenodata_df = pd.read_csv('/iblm/netapp/home/karthik/gasperini_project/data/phenodata.txt', sep=' ', names=colnames)
 
-# drop nan values from dataframe (cannot parse strings)
-phenodata_df = phenodata_df.dropna()
-
-def get_guide_names(guides):
-    '''regex function to separate guide names'''
-    
-    guide_list = re.findall(r"[A-Za-z0-9]*\_TSS|chr[A-Z0-9]{1,2}\.\w*top_two|chr[A-Z0-9]{1,2}\.\w*second_two", guides)
-    return guide_list
-
-
-# get guide names and split guide sequences for each cell 
-phenodata_df['guides'] = phenodata_df['gene'].apply(get_guide_names)
+# get guide names and split guide sequences for each cell
+print('getting guide sequences')
+phenodata_df['barcode'] = phenodata_df['barcode'].fillna('')
 phenodata_df['sequences'] = phenodata_df['barcode'].str.split('_')
 
 # select necessary columns from dataframe
-phenodata_df = phenodata_df[['cell', 'guides', 'sequences']]
+phenodata_df = phenodata_df[['cell', 'sequences']]
 
 # get list of guide sequences and cell names (slow step)
 guide_sequences = pd.Series(phenodata_df['sequences'].sum()).unique()
@@ -41,10 +27,12 @@ def guides_present(cell_guide_list):
 
 
 # generate cell guide matrix and transpose matrix so columns are cells
+print('determining guides present in each cell')
 cell_guide_matrix = phenodata_df['sequences'].apply(guides_present)
 cell_guide_matrix.index = cell_names
 cell_guide_matrix.columns = guide_sequences
 cell_guide_matrix = cell_guide_matrix.T
 
 # write output to h5 file (for storing large data)
-cell_guide_matrix.to_hdf('/iblm/netapp/home/karthik/crisprQTL/gasperini_data/cell_guide_matrix.h5', key='df', mode='w')
+print('writing to csv')
+cell_guide_matrix.to_csv('/iblm/netapp/data1/external/Gasperini2019/processed/cell_guide_matrix.csv')
