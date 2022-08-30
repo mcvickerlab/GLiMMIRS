b0f655beacd8ec013f0c8781588e055dc25972a0
somet3000 <karthikguruvayurappan@gmail.com>
2022-08-30 11:01:21 -0700

    Generalized indicator vector computing method

---
+++ scripts/run_model_experimental_suppl_data_table_2_enhancer_pairs.R
@@ -0,0 +1,6 @@
+# This program runs a generalized linear model on enhancer pairs determined by analyzing
+# the 664 enhancer-gene pairs published in the Gasperini et al. 2019 paper, and looking at
+# enhancers that target the same gene.
+#
+# Author: Karthik Guruvayurappan
+
