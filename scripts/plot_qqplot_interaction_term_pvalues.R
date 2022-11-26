36b1ce99e212b0621d8d426a88fd82f15568e0e9
somet3000 <karthikguruvayurappan@gmail.com>
2022-11-25 21:32:21 -0800

    modified scripts to use pseudocount model

--- scripts/plot_qqplot_interaction_term_pvalues.R
+++ scripts/plot_qqplot_interaction_term_pvalues.R
@@ -7,7 +7,7 @@ library(stats)
 library(BoutrosLab.plotting.general)
 
 # read in interaction term p-values
-enhancer.enhancer.pvalues <- read.csv('/iblm/netapp/data1/external/Gasperini2019/processed/enhancer_enhancer_pairs_suppl_table_2_model.csv')
+enhancer.enhancer.pvalues <- read.csv('/iblm/netapp/data1/external/Gasperini2019/processed/enhancer_enhancer_pairs_suppl_table_2_pseudocount_model.csv')
 interaction.pvalues <- enhancer.enhancer.pvalues$interaction.pvalue.list
 interaction.pvalues <- interaction.pvalues[complete.cases(interaction.pvalues)]
 interaction.pvalues <- data.frame(interaction.pvalues[order(interaction.pvalues)])
@@ -19,8 +19,8 @@ interaction.pvalues$unif <- -log10(interaction.pvalues$unif)
 create.scatterplot(
     formula = interaction.pvalue ~ unif,
     data = interaction.pvalues,
-    filename = '/iblm/netapp/home/karthik/crisprQTL/plots/interaction_term_qqplot.png',
-    resolution = 200,
+    filename = '/iblm/netapp/home/karthik/crisprQTL/plots/interaction_term_qqplot.tiff',
+    resolution = 300,
     add.xyline = TRUE,
     xlab.label = '-log10(expected pvalue)',
     ylab.label = '-log10(observed pvalue)'
--- scripts/plot_qqplot_interaction_term_pvalues_at_scale.R
+++ scripts/plot_qqplot_interaction_term_pvalues_at_scale.R
@@ -7,7 +7,7 @@ library(stats)
 library(BoutrosLab.plotting.general)
 
 # read in interaction term p-values
-enhancer.enhancer.pvalues <- read.csv('/iblm/netapp/data1/external/Gasperini2019/processed/enhancer_enhancer_at_scale_20_cells_model.csv')
+enhancer.enhancer.pvalues <- read.csv('/iblm/netapp/data1/external/Gasperini2019/processed/enhancer_enhancer_at_scale_20_cells_pseudocount_model.csv')
 interaction.pvalues <- enhancer.enhancer.pvalues$interaction.pvalue.list
 interaction.pvalues <- interaction.pvalues[complete.cases(interaction.pvalues)]
 interaction.pvalues <- data.frame(interaction.pvalues[order(interaction.pvalues)])
@@ -21,8 +21,8 @@ interaction.pvalues$unif <- -log10(interaction.pvalues$unif)
 create.scatterplot(
     formula = interaction.pvalue ~ unif,
     data = interaction.pvalues,
-    filename = '/iblm/netapp/home/karthik/crisprQTL/plots/interaction_term_at_scale_qqplot.png',
-    resolution = 200,
+    filename = '/iblm/netapp/home/karthik/crisprQTL/plots/interaction_term_at_scale_qqplot.tiff',
+    resolution = 300,
     add.xyline = TRUE,
     xlab.label = '-log10(expected pvalue)',
     ylab.label = '-log10(observed pvalue)'
