8600ecadcf14874017506cb85d05ab2ad0072bd8
somet3000 <karthikguruvayurappan@gmail.com>
2022-12-21 11:52:20 -0800

    Deleted FlashFry and Rplots files

--- scripts/compute_flashfry_guide_offtargets.sh
+++
@@ -1,6 +0,0 @@
-java -Xmx4g -jar /iblm/netapp/home/karthik/FlashFry/FlashFry-assembly-1.15.jar \
- discover \
- --database /iblm/netapp/home/karthik/gasperini_project/flashfry/hg19 \
- --fasta /iblm/netapp/home/karthik/FlashFry/Gasperini2019/guide_sequences.fasta \
- --output /iblm/netapp/home/karthik/FlashFry/Gasperini2019/guide_sequences.fasta.off_targets \
- --flankingSequence 0
--- scripts/create_flashfry_hg19_index.sh
+++
@@ -1,5 +0,0 @@
-java -Xmx6000m -jar /iblm/netapp/home/karthik/FlashFry/FlashFry-assembly-1.15.jar \
-index \
---tmpLocation /iblm/netapp/home/karthik/FlashFry \
---database /iblm/netapp/home/karthik/FlashFry/databases/hg19 \
---reference /iblm/netapp/data1/external/UCSC/hg19.fa
