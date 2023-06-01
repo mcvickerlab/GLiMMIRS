b782ea4e29f01421174b3c7f1711b396735b2fcc
somet3000 <karthikguruvayurappan@gmail.com>
2023-06-01 11:03:02 -0700

    fixed bugs in data download script

--- src/experimental/data/download_experimental_data.sh
+++ src/experimental/data/download_experimental_data.sh
@@ -11,12 +11,12 @@ mkdir data/experimental/interim/
 mkdir data/experimental/processed/
 
 # download supplementary table 2 from Cell website
-wget https://ars.els-cdn.com/content/image/1-s2.0-S009286741831554X-mmc2.xlsx -P data/raw/
+wget https://ars.els-cdn.com/content/image/1-s2.0-S009286741831554X-mmc2.xlsx -P data/experimental/raw/
 
 # download at-scale expression matrix, cell barcodes, and gene names
-wget https://ftp.ncbi.nlm.nih.gov/geo/series/GSE120nnn/GSE120861/suppl/GSE120861_at_scale_screen.exprs.mtx.gz -P data/raw/
-wget https://ftp.ncbi.nlm.nih.gov/geo/series/GSE120nnn/GSE120861/suppl/GSE120861_at_scale_screen.genes.txt.gz -P data/raw/
-wget https://ftp.ncbi.nlm.nih.gov/geo/series/GSE120nnn/GSE120861/suppl/GSE120861_at_scale_screen.cells.txt.gz -P data/raw/
+wget https://ftp.ncbi.nlm.nih.gov/geo/series/GSE120nnn/GSE120861/suppl/GSE120861_at_scale_screen.exprs.mtx.gz -P data/experimental/raw/
+wget https://ftp.ncbi.nlm.nih.gov/geo/series/GSE120nnn/GSE120861/suppl/GSE120861_at_scale_screen.genes.txt.gz -P data/experimental/raw/
+wget https://ftp.ncbi.nlm.nih.gov/geo/series/GSE120nnn/GSE120861/suppl/GSE120861_at_scale_screen.cells.txt.gz -P data/experimental/raw/
 
 # download at-scale phenodata for covariate information
-wget https://ftp.ncbi.nlm.nih.gov/geo/series/GSE120nnn/GSE120861/suppl/GSE120861_at_scale_screen.phenoData.txt.gz -P data/raw/
+wget https://ftp.ncbi.nlm.nih.gov/geo/series/GSE120nnn/GSE120861/suppl/GSE120861_at_scale_screen.phenoData.txt.gz -P data/experimental/raw/
