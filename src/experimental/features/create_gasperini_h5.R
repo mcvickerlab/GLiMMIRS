80c0087787d038072768ca6b3df262bc7df2e997
somet3000 <karthikguruvayurappan@gmail.com>
2023-12-19 13:37:20 -0800

    created conda environment for h5 files

---
+++ src/experimental/features/create_gasperini_h5.R
@@ -0,0 +1,7 @@
+# This script creates a single HDF5 data structure that contains all of the
+# Gasperini data tables and matrices for easy downstream modeling.
+#
+# Author: Karthik Guruvayurappan
+
+library(Matrix)
+library(rhdf5)
