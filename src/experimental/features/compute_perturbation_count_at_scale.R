49e23c28a7a30e9462f015f7d04d51d21471a74f
somet3000 <karthikguruvayurappan@gmail.com>
2024-01-09 10:46:19 -0800

    bug fix for matrix subsetting

--- src/experimental/features/compute_perturbation_count_at_scale.R
+++ src/experimental/features/compute_perturbation_count_at_scale.R
@@ -65,31 +65,35 @@ for (i in 1:nrow(enhancer.pairs)) {
     enh.1.guides <- enh.1.guides[enh.1.guides %in% guide.names]
     enh.2.guides <- enh.2.guides[enh.2.guides %in% guide.names]
 
-    # ensure that there are guides for each enhancer to compute perturbations
-    if ((length(enh.1.guides) > 0) & length(enh.2.guides) > 0) {
+    # compute total perturbation vectors for each enhancer
+    enh.1.perturb <- guide.matrix[enh.1.guides, ]
+    enh.2.perturb <- guide.matrix[enh.2.guides, ]
 
-        # compute total perturbation vectors for each enhancer
-        enh.1.perturb <- colSums(guide.matrix[enh.1.guides, ])
-        enh.2.perturb <- colSums(guide.matrix[enh.2.guides, ])
-
-        # set max perturbation to 1
-        enh.1.perturb[enh.1.perturb > 0] <- 1
-        enh.2.perturb[enh.2.perturb > 0] <- 1
-
-        # compute output metrics
-        gene <- enhancer.pairs$gene[i]
-        enh.1.count <- sum(enh.1.perturb)
-        enh.2.count <- sum(enh.2.perturb)
-        double.count <- sum(enh.1.perturb * enh.2.perturb)
-
-        # store output metrics
-        enh.1.list[i] <- enhancer.1
-        enh.2.list[i] <- enhancer.2
-        gene.list[i] <- gene
-        enh.1.count.list[i] <- enh.1.count
-        enh.2.count.list[i] <- enh.2.count
-        double.count.list[i] <- double.count
+    if (length(enh.1.guides) > 1) {
+        enh.1.perturb <- colSums(enh.1.perturb)
     }
+
+    if (length(enh.2.guides) > 1) {
+        enh.2.perturb <- colSums(enh.2.perturb)
+    }
+
+    # set max perturbation to 1
+    enh.1.perturb[enh.1.perturb > 0] <- 1
+    enh.2.perturb[enh.2.perturb > 0] <- 1
+
+    # compute output metrics
+    gene <- enhancer.pairs$gene[i]
+    enh.1.count <- sum(enh.1.perturb)
+    enh.2.count <- sum(enh.2.perturb)
+    double.count <- sum(enh.1.perturb * enh.2.perturb)
+
+    # store output metrics
+    enh.1.list[i] <- enhancer.1
+    enh.2.list[i] <- enhancer.2
+    gene.list[i] <- gene
+    enh.1.count.list[i] <- enh.1.count
+    enh.2.count.list[i] <- enh.2.count
+    double.count.list[i] <- double.count
 }
 
 # write to output file
