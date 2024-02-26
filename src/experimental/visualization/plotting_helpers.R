77e4ed5ac77c62d1271fe30c469ffe153f3f15c3
somet3000 <karthikguruvayurappan@gmail.com>
2024-02-26 13:35:12 -0800

    ported file naming to helper function

--- src/experimental/visualization/plotting_helpers.R
+++ src/experimental/visualization/plotting_helpers.R
@@ -3,10 +3,18 @@
 #
 # Author: Karthik Guruvayurappan
 
-#' Write plot to .png file using ggsave
+#' Concatenates enhancer pair and gene with underscore in between
 #'
-#' @param plot The ggplot object to be saved to a .png file
+#' @param enhancer_1 Name of enhancer 1.
+#' @param enhancer_2 Name of enhancer 2.
+#' @param gene Name of gene
 #'
-write_plot_png <- function() {
-
+concat_enhancer_pair_name <- function(enhancer_1, enhancer_2, gene) {
+  output <- paste(
+    enhancer_1,
+    enhancer_2,
+    gene,
+    sep = '_'
+  )
+  output
 }
