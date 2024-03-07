765470b78f006446b8930142161fa86da4412472
somet3000 <karthikguruvayurappan@gmail.com>
2024-03-07 07:45:39 -0800

    added code to plot UMIs/cell metric

--- src/experimental/features/compute_n50_and_UMI_props.R
+++ src/experimental/features/compute_n50_and_UMI_props.R
@@ -2,24 +2,29 @@
 # of a gene. This becomes particularly relevant for the enhancer-enhancer
 # interaction models, since a few cells in the double perturbation vector have
 # large counts, which leads to large positive interaction coefficients. This
-# script generates both a N50 style metric and also a UMI proportion based
-# metric for quantifying this effect.
+# script generates a UMI proportion based metric for quantifying this effect.
 #
 # Author: Karthik Guruvayurappan
 
 library(rhdf5)
+source('src/experimental/models/analysis_helpers.R')
 
 # read in gene expression matrix
+h5_name <- 'data/experimental/processed/gasperini_data.h5'
+expr_matrix <- read_expr_matrix(h5_name)
 
 # compute total UMIs per gene
+gene_umi_counts <- rowSums(expr_matrix)
 
-# compute number of cells with a UMI per gene 
+# compute number of cells with a UMI per gene
+cells_with_umi_counts <- rowSums(expr_matrix > 0)
 
 # take log10
+prop_umis_metric <- log10(gene_umi_counts / cells_with_umi_counts)
 
-# compute 50% of number of UMIs
-
-# for each gene, order cells by UMI count
-  # determine the number of cells required to reach 50% of UMIs
-
-# save both metrics to output file for plotting/visualization
+# save metric to output file for plotting/visualization
+write.csv(
+  prop_umis_metric,
+  'data/experimental/processed/umi_cells_metric.csv',
+  quote = FALSE
+)
