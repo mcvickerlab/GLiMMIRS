e53eb5fd2dc53d9f6d7a77cfd1fb4ccdc4724dcf
somet3000 <karthikguruvayurappan@gmail.com>
2024-04-01 13:44:02 -0700

    updated STING-seq bootstrap plots

--- src/sting_seq/visualization/plot_interaction_dotplot.R
+++ src/sting_seq/visualization/plot_interaction_dotplot.R
@@ -26,7 +26,7 @@ for (i in 1:nrow(ptprc.pairs)) {
 
     # read in file with bootstrap estimates
     boot.file <- paste0(
-        '/iblm/netapp/home/karthik/GLiMMIRS/data/experimental/processed/sting_seq_bootstrap/24_01_05_sting_seq_bootstrap_interactions_',
+        '/iblm/netapp/home/karthik/GLiMMIRS/data/gasperini/processed/sting_seq_bootstrap/24_01_05_sting_seq_bootstrap_interactions_',
         snp.1,
         '_',
         snp.2,
@@ -47,7 +47,7 @@ for (i in 1:nrow(ptprc.pairs)) {
     adj.expected <- exp(intercept + enhancer.1 + enhancer.2)
     adj.additive <- exp(intercept) - (exp(intercept) - exp(intercept + enhancer.1)) - (exp(intercept) - exp(intercept + enhancer.2))
 
-    plot.df <- data.frame(cbind(adj.intercept, adj.enhancer.1, adj.enhancer.2, adj.interaction, adj.expected, adj.additive))
+    plot.df <- data.frame(cbind(adj.intercept, adj.enhancer.1, adj.enhancer.2, adj.interaction))
 
     # create plot
     plot.means <- colMeans(plot.df)
@@ -55,21 +55,35 @@ for (i in 1:nrow(ptprc.pairs)) {
     plot.upper <- apply(plot.df, 2, quantile, probs = 0.95)
     plot.df <- data.frame(t(rbind(plot.means, plot.lower, plot.upper)))
     rownames(plot.df) <- c(
-        'No Perturbation',
+        'None',
         'E1',
         'E2',
-        'E1 + E2',
-        'E1 + E2 (mult)',
-        'E1 + E2 (add)'
+        'E1 + E2'
     )
     plot.df$perturbation <- rownames(plot.df)
-    plot.df$perturbation <- factor(plot.df$perturbation, levels=c('No Perturbation', 'E1', 'E2', 'E1 + E2', 'E1 + E2 (mult)', 'E1 + E2 (add)'))
+    plot.df$perturbation <- factor(plot.df$perturbation, levels=c('None', 'E1', 'E2', 'E1 + E2'))
 
     plot <- ggplot(plot.df, aes(x = perturbation, y = plot.means)) +
-        geom_point() +
-        geom_errorbar(aes(ymin = plot.lower, ymax = plot.upper)) +
-        ylab('Scaled Expression') +
-        theme_classic()
+      geom_rect(
+          aes(xmin = -Inf, xmax = Inf, ymin = quantile(adj.expected, 0.05), ymax = quantile(adj.expected, 0.95)),
+          fill = 'darkgray',
+          alpha = 0.6,
+          color = 'black',
+          linetype = 'dashed'
+        ) +
+      geom_pointrange(aes(ymin = plot.lower, ymax = plot.upper), fatten = 10, linewidth = 1) +
+      ylab('Scaled Expression') +
+      theme_classic() +
+      xlab('Perturbation') +
+      theme(
+        axis.line = element_line(linewidth = 1),
+        axis.title.x = element_text(size = 24, color = 'black'),
+        axis.title.y = element_text(size = 24, color = 'black'),
+        axis.text = element_text(size = 20, color = 'black', family = 'Helvetica'),
+        axis.ticks = element_line(color = 'black', linewidth = 1),
+        axis.ticks.length = unit(2, 'mm'),
+        plot.margin = rep(unit(10, 'mm'), 4)
+      )
 
     # write to output file
     ggsave(
@@ -78,12 +92,12 @@ for (i in 1:nrow(ptprc.pairs)) {
             snp.1,
             '_',
             snp.2,
-            '.png'
+            '.pdf'
         ),
         plot = plot,
-        device = 'png'
+        device = 'pdf',
+        width = 7,
+        height = 7,
+        unit = 'in'
     )
 }
-
-
-
