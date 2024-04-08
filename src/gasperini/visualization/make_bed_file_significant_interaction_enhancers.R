ce661558a78dad9a2fdcea39dcc826c0f743c12c
somet3000 <karthikguruvayurappan@gmail.com>
2024-04-08 06:02:37 -0700

    added code to make BED file of significant interaction enhancers

--- src/gasperini/visualization/make_bed_file_significant_interaction_enhancers.R
+++ src/gasperini/visualization/make_bed_file_significant_interaction_enhancers.R
@@ -10,5 +10,39 @@ source('src/gasperini/models/analysis_helpers.R')
 # read in significant enhancer-enhancer interactions
 significant_results <- get_significant_results()
 
+# get enhancers
+enhancers <- c(
+  significant_results$enhancer.1.list,
+  significant_results$enhancer.2.list
+)
+enhancers <- unique(enhancers)
 
+# read in genomic coordinates for enhancers
+enhancer_coords <- read.csv(
+  'data/gasperini/processed/enhancer_coords.csv'
+)
+
+# merge enhancers with enhancer coordinates
+enhancers <- merge(
+  data.frame(enhancers),
+  enhancer_coords,
+  by.x = 'enhancers',
+  by.y = 'gRNAgroup'
+)
+
+# select necessary columns for a BED file
+enhancers <- enhancers[
+  ,
+  c('gRNAgroup.chr', 'gRNAgroup.start', 'gRNAgroup.stop', 'enhancers')
+]
+
+# write to output BED file
+write.table(
+  enhancers,
+  'data/gasperini/processed/significant_interaction_enhancers.bed',
+  sep = '\t',
+  row.names = FALSE,
+  col.names = FALSE,
+  quote = FALSE
+)
 
