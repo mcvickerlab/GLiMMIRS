dc556f9d89d1605d081232279854161f568c21a9
somet3000 <karthikguruvayurappan@gmail.com>
2024-04-12 06:40:36 -0700

    fixed data download bug

--- src/sting_seq/data/download_sting_seq_data.sh
+++ src/sting_seq/data/download_sting_seq_data.sh
@@ -16,7 +16,7 @@ wget -O data/sting_seq/raw/gene_expression/matrix.mtx.gz https://ftp.ncbi.nlm.ni
 
 # download guide matrix from GEO
 mkdir -p data/sting_seq/raw/guide_matrix/
-wget -O data/sting_seq/raw/gene_expression/barcodes.tsv.gz https://ftp.ncbi.nlm.nih.gov/geo/samples/GSM5225nnn/GSM5225859/suppl/GSM5225859%5FSTINGseq%2Dv1%5FGDO.barcodes.tsv.gz
-wget -O data/sting_seq/raw/gene_expression/features.tsv.gz https://ftp.ncbi.nlm.nih.gov/geo/samples/GSM5225nnn/GSM5225859/suppl/GSM5225859%5FSTINGseq%2Dv1%5FGDO.features.tsv.gz
-wget -O data/sting_seq/raw/gene_expression/matrix.mtx.gz https://ftp.ncbi.nlm.nih.gov/geo/samples/GSM5225nnn/GSM5225859/suppl/GSM5225859%5FSTINGseq%2Dv1%5FGDO.matrix.mtx.gz
+wget -O data/sting_seq/raw/guide_matrix/barcodes.tsv.gz https://ftp.ncbi.nlm.nih.gov/geo/samples/GSM5225nnn/GSM5225859/suppl/GSM5225859%5FSTINGseq%2Dv1%5FGDO.barcodes.tsv.gz
+wget -O data/sting_seq/raw/guide_matrix/features.tsv.gz https://ftp.ncbi.nlm.nih.gov/geo/samples/GSM5225nnn/GSM5225859/suppl/GSM5225859%5FSTINGseq%2Dv1%5FGDO.features.tsv.gz
+wget -O data/sting_seq/raw/guide_matrix/matrix.mtx.gz https://ftp.ncbi.nlm.nih.gov/geo/samples/GSM5225nnn/GSM5225859/suppl/GSM5225859%5FSTINGseq%2Dv1%5FGDO.matrix.mtx.gz
 wget -O data/sting_seq/raw/guide_matrix/umi_thresholds.csv.gz https://ftp.ncbi.nlm.nih.gov/geo/samples/GSM5225nnn/GSM5225859/suppl/GSM5225859%5FSTINGseq%2Dv1%5FGDO.umi%5Fthresholds.csv.gz
