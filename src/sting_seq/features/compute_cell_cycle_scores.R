05063df4471be12cf76d3894f96dcff6de1193c9
somet3000 <karthikguruvayurappan@gmail.com>
2024-04-12 06:48:53 -0700

    refactored STING-seq cell cycle code

--- src/sting_seq/features/compute_cell_cycle_scores.R
+++ src/sting_seq/features/compute_cell_cycle_scores.R
@@ -12,73 +12,42 @@ source('src/sting_seq/features/analysis_helpers.R')
 # read in STING-seq expression matrix
 expr_matrix <- read_expr_matrix()
 
-# read in STING-seq expression matrix (for all lanes)
-rna <- Read10X(
-    data.dir = '/iblm/netapp/data1/external/Morris_2023_STING_seq/cDNA_v1/'
-)
-
-# remove '-1' from end of each cell barcode
-colnames(rna) <- substr(colnames(rna), 1, nchar(colnames(rna)) - 2)
-
-# read in supplementary table S3C
-table.s3c <- read.csv(
-    '/iblm/netapp/data1/external/Morris_2023_STING_seq/sting_seq_suppl_table_s3c.csv'
-)
-
-# filter for cells that were included in STING-seq DE analysis
-rna <- rna[, colnames(rna) %in% table.s3c$Cell.Barcode]
-
-
 # use predefined S and G2M gene sets (from Seurat)
-s.genes <- cc.genes$s.genes
-g2m.genes <- cc.genes$g2m.genes
+s_genes <- cc.genes$s.genes
+g2m_genes <- cc.genes$g2m.genes
 
 # run Seurat initialization steps for expression matrix
 rna <- CreateSeuratObject(
-    counts = rna
+  counts = expr_matrix
 )
 rna <- NormalizeData(rna)
 rna <- FindVariableFeatures(
-    rna,
-    selection.method  = "vst"
+  rna,
+  selection.method  = "vst"
 )
 rna <- ScaleData(
-    rna,
-    features = rownames(rna)
+  rna,
+  features = rownames(rna)
 )
 
 # run Seurat cell cycle scoring method
 rna <- CellCycleScoring(
-    rna,
-    s.features = s.genes,
-    g2m.features = g2m.genes,
-    set.ident = TRUE
-)
-
-# run PCA to show cell separation based on cell cycle phase
-rna <- RunPCA(rna, features = c(s.genes, g2m.genes))
-cell.cycle.pca <- DimPlot(rna, raster = FALSE)
-
-# save plot to output file
-ggsave(
-    filename = '/iblm/netapp/home/karthik/GLiMMIRS/out/23_11_14_cell_cycle_pca_sting_seq_v1.png',
-    plot = cell.cycle.pca,
-    device = 'png',
-    height = 7,
-    width = 7,
-    units = 'in'
+  rna,
+  s.features = s_genes,
+  g2m.features = g2m_genes,
+  set.ident = TRUE
 )
 
 # write scores to CSV files
-s.scores <- rna[[]]['S.Score']
-g2m.scores <- rna[[]]['G2M.Score']
+s_scores <- rna[[]]['S.Score']
+g2m_scores <- rna[[]]['G2M.Score']
 
 write.csv(
-    s.scores,
-    '/iblm/netapp/home/karthik/GLiMMIRS/data/experimental/interim/23_11_14_sting_seq_v1_cell_cycle_s_scores.csv'
+  s_scores,
+  'data/sting_seq/interim/cell_cycle_s_scores.csv'
 )
 write.csv(
-    g2m.scores,
-    '/iblm/netapp/home/karthik/GLiMMIRS/data/experimental/interim/23_11_14_sting_seq_v1_cell_cycle_g2m_scores.csv'
+  g2m_scores,
+  'data/sting_seq/interim/cell_cycle_g2m_scores.csv'
 )
 
