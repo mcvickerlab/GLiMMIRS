cf11f19a5893f998ca563ba320f076fb4e33b133
somet3000 <karthikguruvayurappan@gmail.com>
2024-05-21 07:16:08 -0700

    added plots for example prior and posterior distribution

--- src/gasperini/visualization/plot_interaction_detection.R
+++ src/gasperini/visualization/plot_interaction_detection.R
@@ -25,10 +25,111 @@ calc_mean_sd <- function(alpha_val, beta_val) {
   return(list(mean=mean_val, sd=sd_val, alpha=alpha_val, beta=beta_val))
 }
 
+###############################################################################
+# Plot example prior and posterior representing Gasperini
+###############################################################################
+# define parameters for prior distribution
+prior_interaction_frequency <- 0.25
+prior_interaction_sd <- 0.1
+prior_params <- calc_alpha_beta(
+  prior_interaction_frequency,
+  prior_interaction_sd
+)
+
+# define power analogous to Gasperini
+power <- 0.1
+
+# define number of tests and number of significant hits
+num_tests <- 46166
+num_signif <- 11
+
+# define parameters for prior detection distribution
+prior_detect_mean <- power * prior_interaction_frequency
+prior_detect_sd <- power * prior_interaction_sd
+prior_detect_params <- calc_alpha_beta(
+  prior_detect_mean,
+  prior_detect_sd
+)
+
+# define parameters for posterior detection distribution
+post_detect_alpha <- prior_detect_params$alpha + num_signif
+post_detect_beta <- num_tests - num_signif + prior_detect_params$beta
+post_detect_params <- calc_mean_sd(post_detect_alpha, post_detect_beta)
+
+# make plot of example prior
+x <- seq(0.000001, 0.08, by = 0.000001)
+prior_detect_densities <- dbeta(
+  x, 
+  prior_detect_params$alpha,
+  prior_detect_params$beta
+)
+plot_df <- data.frame(cbind(
+  x,
+  prior_detect_densities
+))
+plot <- ggplot(plot_df, aes(x = x, y = prior_detect_densities)) +
+  geom_line(linewidth = 1, color = 'black') +
+  theme_classic() +
+  xlab('') +
+  ylab('Prior Detection Density') +
+  scale_x_continuous(expand = c(0.02, 0)) +
+  scale_y_continuous(expand = c(0.02, 0)) +
+  theme(
+    axis.line = element_line(linewidth = 1),
+    axis.title.x = element_text(size = 24, color = 'black'),
+    axis.title.y = element_text(size = 24, color = 'black'),
+    axis.text = element_text(size = 24, color = 'black'),
+    axis.ticks = element_line(color = 'black', linewidth = 1),
+    axis.ticks.length = unit(2, 'mm'),
+    plot.margin = rep(unit(10, 'mm'), 4)
+  )
+ggsave(
+  plot = plot,
+  filename = 'out/example_prior_distribution.pdf',
+  device = 'pdf'
+)
+
+# make plot of example posterior
+x <- seq(0.000001, 0.005, by = 0.000001)
+post_detect_densities <- dbeta(
+  x,
+  post_detect_alpha,
+  post_detect_beta
+)
+plot_df <- data.frame(cbind(
+  x,
+  post_detect_densities
+))
+plot <- ggplot(plot_df, aes(x = x, y = post_detect_densities)) +
+  geom_line(linewidth = 1, color = 'black') +
+  theme_classic() +
+  xlab('') +
+  ylab('Posterior Detection Density') +
+  scale_x_continuous(expand = c(0.02, 0)) +
+  scale_y_continuous(expand = c(0.02, 0)) +
+  theme(
+    axis.line = element_line(linewidth = 1),
+    axis.title.x = element_text(size = 24, color = 'black'),
+    axis.title.y = element_text(size = 24, color = 'black'),
+    axis.text = element_text(size = 24, color = 'black'),
+    axis.ticks = element_line(color = 'black', linewidth = 1),
+    axis.ticks.length = unit(2, 'mm'),
+    plot.margin = rep(unit(10, 'mm'), 4)
+  )
+ggsave(
+  plot = plot,
+  filename = 'out/example_posterior_distribution.pdf',
+  device = 'pdf'
+)
+
+###############################################################################
+# Plot Bayesian curves for multiple powers + priors
+###############################################################################
+
 # define prior interaction frequencies
 prior_interaction_frequencies <- seq(0.05, 1, length.out = 20)
 prior_interaction_sds <- rep(
-  0.025,
+  0.1,
   length(prior_interaction_frequencies)
 )
 
@@ -61,10 +162,10 @@ for (i in 1:length(prior_interaction_frequencies)) {
   prior_results_df$prior <- prior_interaction_frequency
 
   # define number of pairs tested for interactions
-  num_tests <- 264
+  num_tests <- 46166
 
   # define number of significant interactions
-  num_signif <- 0
+  num_signif <- 11
 
   # iterate through different powers
   for (j in 1:nrow(prior_results_df)) {
@@ -87,6 +188,8 @@ for (i in 1:length(prior_interaction_frequencies)) {
 
     # rescale mean of posterior by power
     prior_results_df$posterior_mean[j] <- post_detect_params$mean / power
+
+
   }
 
   # append to larger results data frame
@@ -120,6 +223,6 @@ plot <- ggplot(results_df, aes(x = prior, y = posterior_mean, group = power, col
 
 ggsave(
   plot = plot,
-  filename = 'out/high_confidence_bayesian_interaction_detection.pdf',
+  filename = 'out/at_scale_bayesian_interaction_detection.pdf',
   device = 'pdf'
 )
